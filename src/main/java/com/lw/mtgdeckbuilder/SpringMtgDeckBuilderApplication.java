6329e87321861f15ded569b3344afdcf1059195d
LukeWebster89 <webstarsince89@hotmail.co.uk>
2022-03-14 12:58:02 +0000

    added documents folder with risk assessment

---
+++ src/main/java/com/lw/mtgdeckbuilder/SpringMtgDeckBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.lw.mtgdeckbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMtgDeckBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMtgDeckBuilderApplication.class, args);
+	}
+
+}
